3dd13b7a8f75a60a322c474fbbedac4c96b4b33c
fazekasistvan2022d-wq <fazekas.istvan.2022d@telekimezotur.hu>
2026-09-24 09:30:17 +0200

    Add project files.

---
+++ CegApp/Alkalmazott.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CegApp
+{
+    internal class Alkalmazott
+    {
+        public string Nev { get; set; }
+        protected int Alapber { get; set; }
+        
+        public Alkalmazott(string nev, int alapber)
+        {
+            Nev = nev;
+            Alapber = alapber;
+        }
+
+        public virtual int FizetesSzamitas()
+        {
+            return Alapber;
+        }
+
+        public override string ToString()
+        {
+            return $"Név:{Nev},Fizetés: {FizetesSzamitas()}";
+        }
+    }
+}
